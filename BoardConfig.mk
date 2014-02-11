59f9988494c3953a556ae8909d3e45ff4c36cfec
hroark13 <hroark13@dev01.(none)>
2014-02-11 16:24:36 -0500

    fix ramdisk offset

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,10 +13,11 @@ TARGET_CPU_VARIANT := cortex-a7
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := metis
-
+TARGET_PREBUILT_KERNEL := device/zte/metis/kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # fix this up by examining /proc/mtd on a running device
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
@@ -25,7 +26,7 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/zte/metis/kernel
+
 
 
 # Recovery
--- README.md
+++ README.md
@@ -5,10 +5,3 @@ You can download the built version here
 http://androidforums.com/zte/828110-clockworkmod-zte-radiant-z740.html
 
 
-The recovery image that gets created needs to be modded before the phone will boot it, you can use a hex editor or run this command
-
-printf '\x82\' | dd of=recovery.img bs=1 seek=23 count=1 conv=notrunc
-
-I will try to get this mod automated at some later date
-
-
