12940866a99be65b1b373a6dc422508c2e00ac02
hroark13 <hroark13@dev01.(none)>
2014-02-11 15:17:25 -0500

    basic tree for CWM

---
+++ BoardConfig.mk
@@ -0,0 +1,42 @@
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/zte/metis/BoardConfigVendor.mk
+
+TARGET_ARCH := arm
+TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := unknown
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a7
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := metis
+
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_BASE := 0x80200000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_PREBUILT_KERNEL := device/zte/metis/kernel
+
+
+# Recovery
+TARGET_RECOVERY_INITRC := device/zte/metis/recovery.rc
+TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/metis/rkernel
+RECOVERY_FSTAB_VERSION = 2
+TARGET_RECOVERY_FSTAB := device/zte/metis/recovery.fstab
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
+
+# USB Mass Storage
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
+BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := metis
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/zte/metis/device_metis.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := metis
+PRODUCT_NAME := cm_metis
+PRODUCT_BRAND := zte
+PRODUCT_MODEL := metis
+PRODUCT_MANUFACTURER := zte
